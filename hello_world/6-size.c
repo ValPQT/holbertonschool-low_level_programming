2d5ad347d01e4ffa87d6d7d1e62d1f770610f081
ValPQT <valentinpasquiet@gmail.com>
2025-10-21 12:12:49 +0000

    pas encore fini

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ *main - Entry point 
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char=?")\n
+		printf("Size of an int=?")\n
+		printf("Size of a long int=?")\n
+		printf("Size of a long long int=?")\n
+		printf("Size of a float=?")\n
+}

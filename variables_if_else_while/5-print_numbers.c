f04608431ee06bd95c04f747004759939008ff05
ValPQT <valentinpasquiet@gmail.com>
2025-10-22 21:21:28 +0000

    afficher les chiffres de 0 a 9

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -13,9 +13,9 @@ int main(void)
 {
 	char num;
 
-	for (num = '0'; num <= '9'; num++)
+	for (num = 0 ; num <= 9 ; num++)
 	        {
-			printf(%d ; num);
+			printf("%d"; num);
 		}
 
 	putchar('\n')

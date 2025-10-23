f14068cbd9dee2fc6d0d4de515cdd62ca1c19891
ValPQT <valentinpasquiet@gmail.com>
2025-10-23 19:37:15 +0000

    1, 2, 3,...

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -4,17 +4,17 @@
 int main(void)
 {
 
-  int num;
+	int num;
 
-  for (num = 0; num <= 9; num++)
-  {
-	  putchar(num + '0');
-      if (num != 9)
-      {
-          putchar(',');
-          putchar(' ');
-      }
-  }
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 
 putchar('\n');
 

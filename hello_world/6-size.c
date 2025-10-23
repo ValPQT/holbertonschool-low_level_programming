8271c083536e80347ef7dd29836de20152f30fb7
ValPQT <valentinpasquiet@gmail.com>
2025-10-23 18:40:18 +0000

    task6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
 
 /**
- *main - Entry point 
+ *main - Entry point
  *
  *Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("Size of a char=?")\n
-		printf("Size of an int=?")\n
-		printf("Size of a long int=?")\n
-		printf("Size of a long long int=?")\n
-		printf("Size of a float=?")\n
+	printf("Size of a char : %lu byte(s)\n", size of (char));
+	printf("Size of an int : %lu byte(s)\n", size of int));
+printf("Size of a long int : %lu byte(s)\n", size of a long int));
+printf("Size of a long long int : %lu byte(s)\n", size of a long long int));
+printf("Size of a float : %lu byte(s)\n", size of a float));
+
+return (0)
+
 }

7315e62064b87422e2e4f8e814a05f8490fabafe
ValPQT <valentinpasquiet@gmail.com>
2025-10-23 18:43:01 +0000

    task6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,10 +8,10 @@
 int main(void)
 {
 	printf("Size of a char : %lu byte(s)\n", size of (char));
-	printf("Size of an int : %lu byte(s)\n", size of int));
-printf("Size of a long int : %lu byte(s)\n", size of a long int));
-printf("Size of a long long int : %lu byte(s)\n", size of a long long int));
-printf("Size of a float : %lu byte(s)\n", size of a float));
+	printf("Size of an int : %lu byte(s)\n", size of (int));
+	printf("Size of a long int : %lu byte(s)\n", size of  (long int));
+	printf("Size of a long long int : %lu byte(s)\n", size of (long long int));
+	printf("Size of a float : %lu byte(s)\n", size of (float));
 
 return (0);
 

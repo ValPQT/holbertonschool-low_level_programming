49c3b345b8058e00f29035ecfea32069fd7fd43b
ValPQT <valentinpasquiet@gmail.com>
2025-10-23 19:10:44 +0000

    task7

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -2,20 +2,18 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *main - Entry point
+ * main - Prints the alphabet in lowercase in reverse.
  *
- *Returns: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char letter;
 
-	for (letter = 'z' ; letter >= 'a' ; letter--)
+	for (letter = 'z'; letter >= 'a'; --letter)
 	{
 		putchar(letter);
 	}
-
 	putchar('\n');
-
 	return (0);
 }

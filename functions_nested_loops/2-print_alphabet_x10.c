3f0d00ea63a0a409c3b1dc0c082f5947c6f1c6a5
ValPQT <valentinpasquiet@gmail.com>
2025-10-24 16:28:57 +0000

    alphabetx10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
+/**
+ *main - print the alphabetx10
+ *
+ *Return: Always 0 (Success)
+ */
 void print_alphabet_x10(void)
 {
 	int i;
 	char l;
+
 	for (i = 0; i < 10; i++)
 	{
-		for(l ='a' ; l <='z' ; l++)
+		for (l = 'a' ; l <= 'z' ; l++)
 		{
 			_putchar(l);
 		}

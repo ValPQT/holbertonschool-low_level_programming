cc1d02a71ccd2ebcfc241cdc475ec3256a021320
ValPQT <valentinpasquiet@gmail.com>
2025-10-24 12:32:06 +0000

    alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,10 +5,10 @@
  */
 void print_alphabet(void)
 {
-    char A;
-    for (A = 'a'; A <= 'z'; A++)
-    {
-        _putchar(A);
-    }
-    _putchar('\n');
+	char A;
+	for (A = 'a'; A <= 'z'; A++)
+	{
+		_putchar(A);
+	}
+	_putchar('\n');
 }

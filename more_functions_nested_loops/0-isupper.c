8c6c600fa3aab2eb223b9702a55593580b70a155
ValPQT <valentinpasquiet@gmail.com>
2025-10-27 10:26:46 +0000

    test lettre majuscule

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - check if the given character is upper case.
+ * @c: character to be tested
+ * Return: 1 (True) else 0 (False)
+ *
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	return (0);
+}

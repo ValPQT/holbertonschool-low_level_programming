718239ed6f8cde40baecd31e39c496f0d9006258
ValPQT <valentinpasquiet@gmail.com>
2025-10-28 17:55:36 +0000

    square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,5 +1,8 @@
 #include "main.h"
-
+/**
+ * print_square - prints a square, followed by a new line;
+ * @size: size of the square
+ */
 void print_square(int size)
 {
 	if (size <= 0)
@@ -13,7 +16,7 @@ void print_square(int size)
 
 		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < size; i++)
+			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
 			}

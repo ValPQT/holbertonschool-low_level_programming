2a616ef7d4e82d640cd6a7a3c99746562c35f1bd
ValPQT <valentinpasquiet@gmail.com>
2025-10-28 09:53:23 +0000

    afficher une ligne dans le terminal

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -5,14 +5,12 @@ void print_line(int n)
 	if (n <= 0)
 	{
 	        _putchar('\n');
-	}
-	else
+	}else
 	{
-		int i;
-			for (i = 1; i >= n; i++)
-			{
-				_putchar('_');
-			}
-			_putchar('\n');
+		for ( ; n > 0; n++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
 }

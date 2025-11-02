972a070040997113d6b987b7f042717a8c18f7c0
ValPQT <valentinpasquiet@gmail.com>
2025-11-02 20:21:58 +0000

    afficher la moiti de la string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	for ( j = 5; j < i; j++)
+	{
+		_putchar(str[j]);
+	}
+
+	_putchar('\n');
+}

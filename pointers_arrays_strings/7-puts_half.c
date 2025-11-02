4abdc964c2c49bd2c6430550a6f01e7b12d74240
ValPQT <valentinpasquiet@gmail.com>
2025-11-02 20:32:16 +0000

    afficher la moitie de la string

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -6,17 +6,27 @@
  */
 void puts_half(char *str)
 {
-	int i = 0;
-	int j;
+	int i, j, k;
+
+	i = 0;
 
 	while (str[i] != '\0')
 	{
 		i++;
 	}
-	for ( j = 5; j < i; j++)
-	{
-		_putchar(str[j]);
-	}
 
+	if (i % 2 == 0)
+	{
+		for (k = i / 2; str[k] != '\0'; k++)
+		{
+			_putchar(str[k]);
+		}
+	} else if (i % 2)
+	{
+		for (j = (i - 1) / 2; j < i - 1; j++)
+		{
+			_putchar(str[j + 1]);
+		}
+	}
 	_putchar('\n');
 }

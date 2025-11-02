c6eb280761b295730f886c8b60dfc842cc3b5d09
ValPQT <valentinpasquiet@gmail.com>
2025-11-02 20:03:06 +0000

    afficher 2 par 2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void puts2(char *str);
+{
+	int i, j;
+
+	i = 0;
+
+	while(str[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; j < i; i += 2)
+	{
+		_putchar(str[j]);
+	}
+        _putchar('\n');
+}

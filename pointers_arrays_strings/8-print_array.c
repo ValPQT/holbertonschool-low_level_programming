76d5c21d8d5c18cce04d125ad94b78cc6ae14b0e
ValPQT <valentinpasquiet@gmail.com>
2025-11-02 21:12:18 +0000

    afficher les elements d'un tableau

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * print_array - prints n elements of an array of integers
+ * followed by a new line
+ * @a: array to be printed
+ * @n: number of elements to print
+ */
 void print_array(int *a, int n)
 {
 	int i;

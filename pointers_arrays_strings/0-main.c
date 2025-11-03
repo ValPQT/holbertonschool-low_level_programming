9062a1c3edd1d938afbef4ef8b391168f64bbee6
ValPQT <valentinpasquiet@gmail.com>
2025-11-03 21:31:50 +0000

    task 0

--- pointers_arrays_strings/0-main.c
+++ pointers_arrays_strings/0-main.c
@@ -5,7 +5,7 @@
  * main - check the code
  *
  * Return: Always 0.
- */
+ 
 int main(void)
 {
     int n;
@@ -14,5 +14,42 @@ int main(void)
     printf("n=%d\n", n);
     reset_to_98(&n);
     printf("n=%d\n", n);
+    return (0);
+}
+*/
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+        unsigned int i;
+
+        i = 0;
+        while (i < size)
+        {
+                if (i % 10)
+                {
+                        printf(" ");
+                }
+                if (!(i % 10) && i)
+                {
+                        printf("\n");
+                }
+                printf("0x%02x", buffer[i]);
+                i++;
+        }
+        printf("\n");
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char buffer[98] = {0x00};
+
+    simple_print_buffer(buffer, 98);
+    _memset(buffer, 0x01, 95);
+    printf("-------------------------------------------------\n");
+    simple_print_buffer(buffer, 98);
     return (0);
 }

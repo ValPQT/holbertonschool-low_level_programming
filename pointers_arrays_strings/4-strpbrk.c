f0258d57e7f30e3a0b1e58b29c143bd674ea7e6f
ValPQT <valentinpasquiet@gmail.com>
2025-11-06 20:13:35 +0000

    ensemble de caracteres dans une chaine

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -5,16 +5,17 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
 
-	for (i = 0; *s != '\0' ; i++)
+	for (i = 0; *s != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (*s == accept[j])
 			{
-				return(s);
+				return (s);
 			}
-			s++;
 		}
+		s++;
 	}
+
 	return (NULL);
 }

7c38ec0016ce2e9cd9ea56c5e7b5b06e642443fd
ValPQT <valentinpasquiet@gmail.com>
2025-11-06 21:07:09 +0000

    localiser une sous-chaine

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -16,3 +16,13 @@ int main(void)
     return (0);
 }
 */
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
+
+    t = _strstr(s, f);
+    printf("%s\n", t);
+    return (0);
+}
--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * *_strstr - locates a substring
+ * @haystack: string to search in
+ * @needle: substring to look for
+ *
+ * Return: pointer to the beginning of the located substring
+ * or NULL if the substring is not found
+ */
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
@@ -9,7 +16,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (haystack[i+j] != needle[j])
+			if (haystack[i + j] != needle[j])
 				break;
 		}
 		if (!needle[j])

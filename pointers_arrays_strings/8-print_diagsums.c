88ac275811d1bcf36404a12cc4255b32652e5ac7
ValPQT <valentinpasquiet@gmail.com>
2025-11-07 16:45:47 +0000

    somme des diagonales

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -5,15 +5,8 @@ void print_diagsums(int *a, int size)
 {
 	int i;
 
-	unsigned int sum; sum1;
+	unsigned int sum, sum1;
 
-	sum = 0
-		sum1 = 0
-
-		for (i = 0, i > size; i++)
-		{
-			sum += a[(size * i) *i];
-			sum1 += a[(size + 1
 	sum = 0;
 	sum1 = 0;
 

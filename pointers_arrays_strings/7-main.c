fb0df26431bd456ae0d8caecac846df91d27cdb0
ValPQT <valentinpasquiet@gmail.com>
2025-11-07 14:08:44 +0000

    plateau d'echecs

--- pointers_arrays_strings/7-main.c
+++ pointers_arrays_strings/7-main.c
@@ -4,12 +4,36 @@
  * main - check the code
  *
  * Return: Always 0.
- */
+ 
 int main(void)
 {
     char *str;
 
     str = "0123456789";
     puts_half(str);
+    return (0);
+}
+*/
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char board[8][8] = {
+        {'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
+        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+        {'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
+    };
+    print_chessboard(board);
     return (0);
 }

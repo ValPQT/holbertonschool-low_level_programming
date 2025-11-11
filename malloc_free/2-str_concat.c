8a04d942d5277b7a467407c4d9b13d3ead41cd79
ValPQT <valentinpasquiet@gmail.com>
2025-11-11 22:40:24 +0000

    concatener deux chaines

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i, j;
+	char *s3;
+
+	for (i = 0; *s1 != '\0' ; i++)
+	{
+		s1++;
+	}
+	return (i);
+
+	for (j = 0; *s2 != '\0' ; i++)
+	{
+		s2++;
+	}
+	return (j);
+
+	s3 = 
+}

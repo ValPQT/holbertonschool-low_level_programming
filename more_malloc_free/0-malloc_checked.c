48151b4a9638aa8ccf511cd040242e961c4c363d
ValPQT <valentinpasquiet@gmail.com>
2025-11-12 15:13:33 +0000

    task 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+		if (ptr == NULL)
+		    exit(98);
+
+	return (ptr);
+}

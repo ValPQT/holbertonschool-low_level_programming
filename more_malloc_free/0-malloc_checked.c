8855143518dbdf34e83f696e67de25656b850e35
ValPQT <valentinpasquiet@gmail.com>
2025-11-12 15:14:56 +0000

    task 0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -3,6 +3,12 @@
 #include <stdlib.h>
 #include <limits.h>
 
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;

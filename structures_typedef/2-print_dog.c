ba058c2436cd5fa145048b7f5f1e039b2271fd6c
ValPQT <valentinpasquiet@gmail.com>
2025-11-13 17:42:08 +0000

    afficher struct dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include "dog.h"
 
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initialize
+ */
 void print_dog(struct dog *d)
 {
 	if (d == NULL)

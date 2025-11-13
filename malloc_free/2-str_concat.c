a742135511d38557ac315197620d1a43cd500db4
ValPQT <valentinpasquiet@gmail.com>
2025-11-13 20:33:06 +0000

    concatener deux chaines de caracteres

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,23 +1,42 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int i, j;
 	char *s3;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	for (i = 0; *s1 != '\0' ; i++)
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
+		return (NULL);
+
+	i = 0;
+	j = 0;
+
+	if (s1)
 	{
-		s1++;
+		while (i < len1)
+		{
+			s3[i] = s1[i];
+			i++;
+		}
 	}
-	return (i);
 
-	for (j = 0; *s2 != '\0' ; i++)
+	if (s2)
 	{
-		s2++;
+		while (i < (len1 + len2))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
 	}
-	return (j);
+	s3[i] = '\0';
 
-	s3 = 
+	return (s3);
 }

33918f34ebf4e24b585a7feeae997b953e37e96c
ValPQT <valentinpasquiet@gmail.com>
2025-11-13 19:44:10 +0000

    duplicated

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,7 +1,12 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
+/**
+ * *_strdup - copies the string given as parameter
+ * @str: string to duplicate
+ *
+ * Return: pointer to the copied string (Success), NULL (Error)
+ */
 char *_strdup(char *str)
 {
 	char *dup;
@@ -16,14 +21,13 @@ char *_strdup(char *str)
 	while (str[len])
 		len++;
 
-	dup = malloc(sizeof(char) *(len + 1));
+	dup = malloc(sizeof(char) * (len + 1));
 
-       	if (dup == NULL)
+	if (dup == NULL)
 		return (NULL);
 
 	while ((dup[i] = str[i]) != '\0')
 		i++;
 
 	return (dup);
-
 }

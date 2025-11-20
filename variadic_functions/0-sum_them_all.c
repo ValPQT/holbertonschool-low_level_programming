8874382603fda823b1176be7525c262ada5a15a9
ValPQT <valentinpasquiet@gmail.com>
2025-11-20 22:44:11 +0000

    somme de tous les parametres

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -5,18 +5,15 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list list;
-	unsigned int n;
+	unsigned int i;
 	int sum = 0;
 
-
-		if (n == 0)
-			return 0;
-
-		va_start(list, n);
+	va_start(list, n);
 
 		for (i = 0; i < n; i++)
-		{
-			sum = va_arg(list, n);
-		}
-		va_end(va_list list);
+			sum += va_arg(list, int);
+
+		va_end (list);
+
+		return(sum);
 }

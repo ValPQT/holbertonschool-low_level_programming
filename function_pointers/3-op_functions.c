19729e6d162e8389a87d44937c5ab322ded506fb
ValPQT <valentinpasquiet@gmail.com>
2025-11-21 13:34:30 +0000

    op_functions

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,1 +1,28 @@
-efafaez
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+int op_add(int a, int b);
+{
+	return(a + b);
+}
+
+int op_sub(int a, int b);
+{
+	return(a - b);
+}
+
+int op_mul(int a, int b);
+{
+	return(a * b);
+}
+
+int op_div(int a, int b);
+{
+	return(a / b);
+}
+
+int op_mod(int a, int b);
+{
+	return(a % b);
+}

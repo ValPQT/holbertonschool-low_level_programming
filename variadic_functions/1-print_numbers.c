bb2c6df41e3d311d7e537a00f4f61178352c27ca
ValPQT <valentinpasquiet@gmail.com>
2025-11-21 09:12:40 +0000

    afficher les nombres

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -2,6 +2,12 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 
+/**
+ * print_numbers - prints numbers given as parameters
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;

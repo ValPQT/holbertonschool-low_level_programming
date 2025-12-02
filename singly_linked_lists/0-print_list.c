49ca9081cad35b2377024ac78c5a126f79081530
ValPQT <valentinpasquiet@gmail.com>
2025-12-02 14:26:36 +0000

    fix : <update all betty and compilation issues>

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
+ *
+ * Return: the number of nodes printed
+ */
 size_t print_list(const list_t *h)
 {
 	size_t len;
-	len = 0;
 
 	while (h)
 	{
@@ -15,5 +20,5 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		len++;
 	}
-	return(len);
+	return (len);
 }

0a1551defe36ff2bafc08391bf482ed5ac261528
ValPQT <valentinpasquiet@gmail.com>
2025-12-05 14:40:30 +0000

    fix : <created the description of the file 1-main.c>

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * dlistint_len - returns the number of elements in
+ * a double linked list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t nodes = 0;

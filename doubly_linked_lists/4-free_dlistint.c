ac33ae88a4a52fc58ee63c76d189387454408261
ValPQT <valentinpasquiet@gmail.com>
2025-12-06 20:38:31 +0000

    feat : <created and added the file 4-free_dlistint.c>

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free (head);
+		head = temp;
+	}
+}

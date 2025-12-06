57983487b7063f12ced46e4bf6298eddd933f2e5
ValPQT <valentinpasquiet@gmail.com>
2025-12-06 21:54:45 +0000

    fix : <udpate compilation issues of the file 6-sum_dlistint.c>

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -5,14 +5,13 @@ int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
 
-	if (head == NULL)
-		return (NULL);
+	if (head)
 
-	while (head != NULL)
-	{
-		sum += head->n;
-		head = head->next;
-	}
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
 
 	return (sum);
 }

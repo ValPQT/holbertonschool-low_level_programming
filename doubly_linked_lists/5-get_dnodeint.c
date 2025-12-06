ff977d1fa514edd43b229018956b344036284e33
ValPQT <valentinpasquiet@gmail.com>
2025-12-06 21:17:08 +0000

    fix : <update betty issues of the file 5-get_dnodeint.c>

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -10,10 +10,10 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	unsigned int i =0;
+	unsigned int i = 0;
 
 	if (head == NULL)
-		return(NULL);
+		return (NULL);
 
 	while (head->prev != NULL)
 		head = head->prev;

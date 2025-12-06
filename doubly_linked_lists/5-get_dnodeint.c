1254b079f4974a989cb6202ab250f1a1943b21e8
ValPQT <valentinpasquiet@gmail.com>
2025-12-06 21:14:57 +0000

    fix : <update the description of the file 5-get_dnodeint.c>

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,13 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * get_dnodeint_at_index - A function that gets a node at index
+ * @head: The pointer to head of list to look through
+ * @index: The index of node to find
+ * Return: A pointer to node if found, or NULL in otherwise
+ */
+
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i =0;

812a19fa0bb53e22a46318bfdd1a900493ae0994
ValPQT <valentinpasquiet@gmail.com>
2025-12-06 21:57:22 +0000

    fix : <udpate description issues of the file 6-sum_dlistint.c>

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * sum_dlistint - A function that sums The elements in the list
+ * @head: The pointer to head of the list
+ * Return: The sum of all elements in the list, or 0 if list is NULL
+ */
+
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;

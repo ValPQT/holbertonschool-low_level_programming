929604af35283ebe05d04b6977126759e916f120
ValPQT <valentinpasquiet@gmail.com>
2025-12-07 21:12:21 +0000

    fix : <updated betty issues of the file 1-create_file.c>

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -3,10 +3,18 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+/**
+ * create_file - A function that creates a file
+ * @filename: The filename to create
+ * @text_content: A NULL terminated string to write to the file
+ * Return: 1 on success, -1 if file can not be created, nor written,
+ * nor write fails.
+ */
+
 int create_file(const char *filename, char *text_content)
 {
 	int fdo, fdw, len = 0;
-	
+
 	if (!filename)
 		return (-1);
 
